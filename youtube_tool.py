803166cf703d4eb7f11bc3a5cd2eba912ec8f20b
Sultan-papagani <69393574+Sultan-papagani@users.noreply.github.com>
2022-06-05 22:47:57 +0200

    Create youtube_tool.py

---
+++ youtube_tool.py
@@ -0,0 +1,32 @@
+from gtts import gTTS
+from pathlib import Path
+from mutagen.mp3 import MP3
+from rich.progress import track
+
+def save_text_to_mp3(reddit_obj):
+    print("Saving Text to MP3 files...")
+
+    length = 0
+
+    # Create a folder for the mp3 files.
+    Path("assets/mp3").mkdir(parents=True, exist_ok=True)
+
+    # video başlığı
+    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False)
+    tts.save(f"assets/mp3/title.mp3")
+    length += MP3(f"assets/mp3/title.mp3").info.length
+
+    #yorumlar
+    for idx, comment in track(enumerate(reddit_obj["comments"]), "Saving..."):
+        # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
+        if length > 50:
+            break
+
+        tts = gTTS(text=comment["comment_body"], lang="en", slow=False)
+
+        tts.save(f"assets/mp3/{idx}.mp3")
+        length += MP3(f"assets/mp3/{idx}.mp3").info.length
+
+    print("Saved Text to MP3 files successfully.")
+    # ! Return the index so we know how many screenshots of comments we need to make.
+    return length, idx

34ed206d061bb8db0bb5046dfe0f68fc90880545
Sultan-papagani <69393574+Sultan-papagani@users.noreply.github.com>
2022-06-14 18:55:13 +0200

    versiyon 2

--- background.py
+++ background.py
@@ -21,10 +21,10 @@ def download_background():
 
     if not Path("assets/mp4/background.mp4").is_file():
         print(
-            "We need to download the Minecraft background video. This is fairly large but it's only done once."
+            "Minecraft arkaplan videosu bulunamadı, bu indirme işlemi 1 kez yapılacaktır."
         )
 
-        print("Downloading the background video... please be patient.")
+        print("Arka plan videosu indiriliyor. çatlamayın, 1 kere yapılacak bir işlem")
 
         ydl_opts = {
             "outtmpl": "assets/mp4/background.mp4",
@@ -35,27 +35,18 @@ def download_background():
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download("https://www.youtube.com/watch?v=n_Dv4JMiwK8")
 
-        print("Background video downloaded successfully!")
+        print("Arka plan videosu başarıyla indirildi")
 
 
 def chop_background_video(video_length):
-    print("Finding a spot in the background video to chop...")
+    print("Video için arkaplan videosu kırpılıyor")
     background = VideoFileClip("assets/mp4/background.mp4")
 
     start_time, end_time = get_start_and_end_times(video_length, background.duration)
-    
-    #ffmpeg_extract_subclip fonksiyonun definitionuna git ve
-    """ cmd = [get_setting("FFMPEG_BINARY"),"-y",
-           "-ss", "%0.2f"%t1,
-           "-i", filename,
-           "-t", "%0.2f"%(t2-t1),
-           "-map", "0", "-vcodec", "copy", targetname] """
-    # cmd değişkenini bunla değiştir
-    
     ffmpeg_extract_subclip(
         "assets/mp4/background.mp4",
         start_time,
         end_time,
         targetname="assets/mp4/clip.mp4",
     )
-    print("Background video chopped successfully!")
+    print("ArkaPlan için video kırpıldı")
